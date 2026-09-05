377a99343ccd7ad6a61cffc4e9e5602dcfc74987
Arockianeopolean <42064263+Arockianeopolean@users.noreply.github.com>
2026-09-05 22:06:31 +0530

    Add files via upload

---
+++ DAY-5/09.Explicit Type Conversion.py
@@ -0,0 +1,27 @@
+# Method - 1
+firstno = (input("Enter First Number:"))
+secondno = (input("Enter Second Number:"))
+print("Addition Result:" , firstno + secondno)
+
+# Method - 2
+firstno = int(input("Enter First Number:"))
+secondno = int(input("Enter Second Number:"))
+print("Addition Result:" , firstno + secondno)
+
+# Method - 3
+firstno = float(input("Enter First Number:"))
+secondno = float(input("Enter Second Number:"))
+print("Addition Result:" , firstno + secondno)
+print("Addition Result:" , round(firstno + secondno))
+
+# Method - 4
+firstno = int(input("Enter First Number:"))
+secondno = float(input("Enter Second Number:"))
+print("Addition Result:" , firstno + secondno)
+
+
+
+
+'''
+name = input("enter the value:")
+print(len(name))
