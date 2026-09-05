ef238cd2b0184e6a2880db324416e69168f225e8
Arockianeopolean <42064263+Arockianeopolean@users.noreply.github.com>
2026-09-05 22:06:02 +0530

    Add files via upload

---
+++ DAY-4/05.Input Statement.py
@@ -0,0 +1,8 @@
+firstname = input("Enter the First Name:") #To Get User's First Name
+#print(firstname)
+#print(type(firstname))
+lastname = input("Enter the Last Name:") #To Get User's Last Name
+#print(type(firstname),type(lastname))
+print("User Name:", firstname + lastname) #without space
+print("User Name:", firstname, lastname) #with space
+print("User Name:", firstname + " " + lastname)
---
+++ DAY-4/06.Output Statement.py
@@ -0,0 +1,12 @@
+a = "Neo"
+b = 20.127
+c = 10
+
+#print(a,b,c)
+#print(a,b,c,sep=";")
+#print(a,b,c,end=",") 
+#print(a,b,c)
+
+print("b=%0.2f" %b)
+print("c=%0.2f" %c)
+print("c=%s" %c)
