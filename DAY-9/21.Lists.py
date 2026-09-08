b7f70e4956e8e40b6518a3c07d40ccffdabd65a6
Arockianeopolean <42064263+Arockianeopolean@users.noreply.github.com>
2026-09-08 12:16:28 +0530

    Update and rename 27.Lists.py to 21.Lists.py

--- DAY-9/21.Lists.py
+++ DAY-9/21.Lists.py
@@ -1,4 +1,3 @@
-'''
 #sample_list = []
 #print(type(sample_list))
 
@@ -19,27 +18,6 @@ if network in list_of_network_devices:
 else:
     print(network + " is not found")
 
-#Looping in Lists
-list_of_network_devices = ["network1","network2","network3"]
-
-#print(range(4))
-
-print("Iterating the list using range() class")
-for index in range(len(list_of_network_devices)): #0,1,2:
-    print(list_of_network_devices[index])
-
-print("Iterating the list using the keyword in")
-for network in list_of_network_devices:
-    print(network)
-
-list_of_network_devices.append("network4")
-print(list_of_network_devices)
-'''
-
-
-
-
-
 sample_list = [2,4,7,10]
 print(max(sample_list))
 
@@ -59,9 +37,3 @@ print(sample_list)
 
 sample_list.reverse()
 print(sample_list)
-
-
-
-
-
-
