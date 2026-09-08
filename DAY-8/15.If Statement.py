5dd9dd3784bcf553f69c484bd9f42a0ed4f58308
Arockianeopolean <42064263+Arockianeopolean@users.noreply.github.com>
2026-09-08 12:10:51 +0530

    Add files via upload

---
+++ DAY-8/15.If Statement.py
@@ -0,0 +1,12 @@
+'''
+a = int(input("Enter the Number:"))
+if(a>0):
+    print("This is a Positive Integer Value:", a)
+print("The End")
+
+'''	
+server = input("Enter the server name:")
+if(server == "localhost"):
+    print("Please enter valid FQDN.")
+
+print("This is end of If condition")
---
+++ DAY-8/16.If Else Statement.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the Number:"))
+if(a%2 == 0):
+	print("This is a Even Integer Value:", a)
+else:
+	print("This is a Odd Integer Value:", a)
