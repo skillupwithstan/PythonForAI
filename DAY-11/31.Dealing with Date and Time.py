bab52cea63e4b3dc8b3633446ed30837e7b2e9c4
Arockianeopolean <42064263+Arockianeopolean@users.noreply.github.com>
2026-09-08 12:29:18 +0530

    Update and rename 40.Dealing with Date and Time.py to 31.Dealing with Date and Time.py

--- DAY-11/31.Dealing with Date and Time.py
+++ DAY-11/31.Dealing with Date and Time.py
@@ -2,8 +2,6 @@ import datetime
 
 #print(dir(datetime))
 
-#print(dir(datetime.date))
-
 # Get the current date and time
 dtnow = datetime.datetime.now()
 print(dtnow)
@@ -17,20 +15,6 @@ print(current_date)
 # Date object to represent a date
 dt = datetime.date(2023, 12, 25)
 print(dt)
-'''
-# Import Only date Class
-from datetime import date
-
-dt = date(2023, 12, 25)
-print(dt)
-
-import datetime
-
-#datetime.date.today()
-
-print(dir(datetime.datetime.day))
-
-help(datetime)
 
 from datetime import date
 
@@ -41,26 +25,6 @@ print("Today's date =", todays_date)
 print("Current year:", todays_date.year)
 print("Current month:", todays_date.month)
 print("Current day:", todays_date.day)
-
-from datetime import time
-
-# time(hour = 0, minute = 0, second = 0)
-a = time()
-print(a)
-
-# time(hour, minute and second)
-b = time(11, 34, 56)
-print(b)
-
-# time(hour, minute and second)
-c = time(hour = 11, minute = 34, second = 56)
-print(c)
-
-# time(hour, minute, second, microsecond)
-d = time(11, 34, 56, 234566)
-print(d)
-
-print(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
 '''
 from datetime import datetime
