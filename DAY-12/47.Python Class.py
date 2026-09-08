3cd30cefd602b3e8808940bd869f5bf2d06020e4
Arockianeopolean <42064263+Arockianeopolean@users.noreply.github.com>
2026-09-08 12:31:13 +0530

    Add files via upload

---
+++ DAY-12/47.Python Class.py
@@ -0,0 +1,87 @@
+'''
+# Sample class with init method
+class Person:
+	# init method or constructor
+	def __init__(self):
+		self.name = "Arockia"
+
+	#def __del__(self):
+	#	print("Object is deleted")
+
+    # Sample Method
+	def printname(self):
+		print('Hello, my name is', self.name)
+		
+#Person().printname()
+
+p = Person()
+p.printname()
+
+p.name = "Raja"
+p.printname()
+
+#del p
+
+print("*****************************************")
+
+'''
+class Addition:
+	# parameterized constructor
+	def __init__(self, f, s):
+		self.first = f
+		self.second = s
+
+	def display(self):
+		print("First number = " + str(self.first))
+		print("Second number = " + str(self.second))
+		print("Addition of two numbers = " + str(self.answer))
+
+	def calculate(self):
+		self.answer = self.first + self.second
+
+# creating object of the class
+obj1 = Addition(10, 20)
+
+# creating second object of same class
+obj2 = Addition(40, 10)
+
+# perform Addition on obj1
+obj1.calculate()
+
+# perform Addition on obj2
+obj2.calculate()
+
+# display result of obj1
+obj1.display()
+
+# display result of obj2
+obj2.display()
+
+print("*********************************************")
+'''
+class Person:
+	# Constructor
+	def __init__(self, name):
+		self.name = name
+
+	# To get name
+	def getName(self):
+		return self.name
+
+	# To check if this person is employee
+	def isEmployee(self):
+		return False
+
+# Inherited or Sub class
+class Employee(Person):
+	# Here we return true
+	def isEmployee(self):
+		return True
+
+emp = Person("Arockia") # An Object of Person
+print(emp.getName(),":", emp.isEmployee())
+
+emp = Employee("Neo") # An Object of Employee
+print(emp.getName(),":", emp.isEmployee())
+
+'''
