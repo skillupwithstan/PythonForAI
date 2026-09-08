7660b9dfd7f7fcaaee99ab58a0f3eded18c5e853
Arockianeopolean <42064263+Arockianeopolean@users.noreply.github.com>
2026-09-08 12:02:13 +0530

    Update Variables.py

--- DAY-3/03.Variables.py
+++ DAY-3/03.Variables.py
@@ -34,32 +34,3 @@ print(type(b))
 a = 10.34324
 b = 12.5
 print(a+b)
-
-
-
-
-
-
-
-
-
-
-
-
-cont = input("Do you want to continue? (yes/no):")
-if(cont == "yes"):
-    print("They are keep continuing")
-elif(cont == "no"):
-    print("No need to continue")
-else:
-    print("Unknown input received from the user!")
-
-'''
-
-
-
-
-#user_input = bool(input("Do you want to continue? (0-Yes, 1-No):"))
-#print(user_input)
-
-
