2edf519dfb76fcfc5e6b1e5f5d540c6d26d93cf5
Arockianeopolean <42064263+Arockianeopolean@users.noreply.github.com>
2026-09-08 12:33:35 +0530

    Update and rename 43.User Defined Functions.py to 32.User Defined Functions.py

---
+++ DAY-12/32.User Defined Functions.py
@@ -0,0 +1,26 @@
+# Function declaration
+def samplePrint():
+    print("This is a sample function!")
+    
+samplePrint() # Calling method
+
+print("***************************")
+
+####################################
+# Function declaration
+def samplePrint(msg):
+    print(msg)
+
+samplePrint("Hello from outside") # Calling method
+
+print("***************************")
+
+'''
+# function with arguments
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    print('Sum: ',sum)
+
+add_numbers(4, 3)
+print("***************************")
+'''
